8b3a87ed37a200ececf74298d5c8c5ccdb9eda83
Everth Paredes <epe0413@gmail.com>
2022-04-10 15:46:28 -0600

    Se agregaron botones de Editar y Eliminar en cada paciente registrado

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -24,7 +24,11 @@ const Paciente = ({paciente}) => {
             Síntomas: {''}
             <span className="font-normal normal-case">{sintomas}</span>
           </p>
-        </div>
+          <div className="flex justify-between mt-10">
+            <button type="button" className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-md">Editar</button>
+            <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-md">Eliminar</button>
+          </div>
+    </div>
   )
 }
 
