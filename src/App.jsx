265a9ebcaa17083b72940096d78dd927cdf2c02f
Everth Paredes <epe0413@gmail.com>
2022-04-11 11:59:22 -0600

    Se agrego funcionalidad para eliminar citas

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id );
+    setPacientes(pacientesActualizados)
+    
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header/>
@@ -22,6 +28,7 @@ function App() {
         <ListadoPacientes
           pacientes={pacientes}
           setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
         />
       </div>
     </div>
